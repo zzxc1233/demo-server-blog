06cc29b6c5689a6ad1cc66214f4d0d66a36ac6dc
champ <champdrm@gmail.com>
2026-02-10 14:24:42 +0700

    feature(api): enhances posts endpoint with search, filter, and pagination

--- routes/postsRouter.mjs
+++ routes/postsRouter.mjs
@@ -5,11 +5,90 @@ import validatePost from "../middleware/postValidation.mjs";
 const postsRouter = Router();
 
 // นักเขียนสามารถดูข้อมูลบทความทั้งหมดได้
-postsRouter.get("/", async (_req, res) => {
-    const result = await connectionPool.query(`SELECT * FROM posts`);
-    return res.status(200).json({
-        data: result.rows
-    });
+postsRouter.get("/", async (req, res) => {
+    try {
+        const category = req.query.category || "";
+        const keyword = req.query.keyword || "";
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 6;
+
+        const safePage = Math.max(1, page);
+        const safeLimit = Math.max(1, Math.min(100, limit));
+        const offset = (safePage - 1) * safeLimit;
+        let query = `
+            SELECT posts.id, posts.image, categories.name AS category, posts.title, posts.description, posts.date, posts.content, statuses.status, posts.likes_count
+            FROM posts
+            INNER JOIN categories ON posts.category_id = categories.id
+            INNER JOIN statuses ON posts.status_id = statuses.id
+    `;
+        let values = [];
+
+        if (category && keyword) {
+            query += `
+                WHERE categories.name ILIKE $1 
+                AND (posts.title ILIKE $2 OR posts.description ILIKE $2 OR posts.content ILIKE $2)`;
+            values = [`%${category}%`, `%${keyword}%`];
+        } else if (category) {
+            query += " WHERE categories.name ILIKE $1";
+            values = [`%${category}%`];
+        } else if (keyword) {
+            query += `
+                WHERE posts.title ILIKE $1 
+                OR posts.description ILIKE $1 
+                OR posts.content ILIKE $1`;
+            values = [`%${keyword}%`];
+        }
+
+        query += ` ORDER BY posts.date DESC LIMIT $${values.length + 1} OFFSET $${values.length + 2
+            }`;
+
+        values.push(safeLimit, offset);
+
+        const result = await connectionPool.query(query, values);
+
+        let countQuery = `
+            SELECT COUNT(*)
+            FROM posts
+            INNER JOIN categories ON posts.category_id = categories.id
+            INNER JOIN statuses ON posts.status_id = statuses.id`;
+        let countValues = values.slice(0, -2); 
+
+        if (category && keyword) {
+            countQuery += `
+        WHERE categories.name ILIKE $1 
+        AND (posts.title ILIKE $2 OR posts.description ILIKE $2 OR posts.content ILIKE $2)`;
+        } else if (category) {
+            countQuery += " WHERE categories.name ILIKE $1";
+        } else if (keyword) {
+            countQuery += `
+                WHERE posts.title ILIKE $1 
+                OR posts.description ILIKE $1 
+                OR posts.content ILIKE $1`;
+        }
+
+        const countResult = await connectionPool.query(countQuery, countValues);
+        const totalPosts = parseInt(countResult.rows[0].count, 10);
+
+        const results = {
+            totalPosts,
+            totalPages: Math.ceil(totalPosts / safeLimit),
+            currentPage: safePage,
+            limit: safeLimit,
+            posts: result.rows,
+        };
+        if (offset + safeLimit < totalPosts) {
+            results.nextPage = safePage + 1;
+        }
+        if (offset > 0) {
+            results.previousPage = safePage - 1;
+        }
+        return res.status(200).json(results);
+    } catch (error) {
+        return res.status(500).json({
+            message: "Server could not read post because database issue",
+            error: error.message
+        });
+    }
 });
 
 // นักเขียนสามารถสร้างบทความได้
@@ -78,7 +157,7 @@ postsRouter.put("/:id", validatePost, async (req, res) => {
     const updatedPost = req.body;
 
     console.log("updatedPost", updatedPost);
-    
+
 
     try {
         const result = await connectionPool.query(
